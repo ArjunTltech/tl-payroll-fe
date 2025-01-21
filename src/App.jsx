ee2e7b787108687a83094c773c3ae530f4bbfce8
arjunTltech <arjunntltechnologies@gmail.com>
2025-01-21 13:04:43 +0530

    add sidebar and nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import { router } from './routes/Routes'
 import { RouterProvider } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import { ThemeProvider } from "@material-tailwind/react";
 
 function App() {
-
+  const customTheme = { 
+    backgroundColor: '#171717',
+   }
 
   return (
     <>
+    <ThemeProvider value={customTheme}>
+
     <RouterProvider router={router} />
+    </ThemeProvider>
     <ToastContainer  position='bottom-right' autoClose='2000'/>
     </>
   )
---
+++ src/components/sidebar/SidebarItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ListItem, ListItemPrefix, ListItemSuffix, Chip } from "@material-tailwind/react";
+import { Link } from 'react-router-dom';
+
+const SidebarItem = ({ icon: Icon, text, badge,to }) => {
+  return (
+    <Link to={to}>
+    <ListItem>
+      <ListItemPrefix>
+        <Icon className="h-5 w-5" />
+      </ListItemPrefix>
+      {text}
+      {badge && (
+        <ListItemSuffix>
+          <Chip value={badge} size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
+        </ListItemSuffix>
+      )}
+    </ListItem>
+    </Link>
+
+  );
+}
+
+export default SidebarItem;
